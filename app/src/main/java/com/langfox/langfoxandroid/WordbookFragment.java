067611364b799cce628a95a3fdd2f90d8a7d53e0
PengInGitHub <felixdeutschland@hotmail.com>
2017-07-22 20:37:32 +0200

    Add back the RecyclerView of loading language into the Wordbook Tab

--- app/src/main/java/com/langfox/langfoxandroid/WordbookFragment.java
+++ app/src/main/java/com/langfox/langfoxandroid/WordbookFragment.java
@@ -1,5 +1,13 @@
 package com.langfox.langfoxandroid;
 
+//source of startActivity() from fragment
+//http://blog.csdn.net/guolin_blog/article/details/8744943
+
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,22 +15,77 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class WordbookFragment extends Fragment {
+public class WordbookFragment extends Fragment implements AdapterView.OnItemClickListener {
+    /**
+     * there is one ListView in the menu layout
+     */
 
-    public WordbookFragment() {
-        // Required empty public constructor
+    private ListView menuList;
+
+    /**
+     * create an adapter for the ListView
+     */
+
+    private ArrayAdapter<String> adapter;
+
+    /**
+     * data to fill up the ListView
+     */
+
+    private String[] menuItems = {"Courses in another language", "Available Courses by Learning Path", "Available Courses by Categories"};
+
+    /**
+     * Initialize the data in adapter when fragment is attached to activity
+     */
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        adapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, menuItems);
     }
 
+    /**
+     * Inflate the layout of this fragment
+     * Set adapter to the ListView
+     */
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wordbook, container, false);
+        View view = inflater.inflate(R.layout.fragment_wordbook, container, false);
+        menuList = (ListView) view.findViewById(R.id.menu_list);
+        menuList.setAdapter(adapter);
+        menuList.setOnItemClickListener(this);
+        return view;
+    }
+
+    /**
+     * deal with onItemClick event
+     * start corresponding Activity
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = null;
+        if (position == 0) {
+            intent = new Intent(getActivity(), WorkbookActivityNewLan.class);
+        } else if (position == 1) {
+            intent = new Intent(getActivity(), WorkbookActivityPath.class);
+        } else if (position == 2) {
+            intent = new Intent(getActivity(), WorkbookActivityCat.class);
+
+        }
+        startActivity(intent);
+
+
     }
 
 
-
+    public WordbookFragment() {
+        // Required empty public constructor
+    }
 }
---
+++ app/src/main/java/com/langfox/langfoxandroid/WorkbookActivityNewLan.java
@@ -0,0 +1,82 @@
+package com.langfox.langfoxandroid;
+
+//use git in Android Studio
+//https://www.coursera.org/learn/internet-of-things-dragonboard/lecture/EfKqi/using-git-with-android-studio
+
+//source
+//https://www.learn2crack.com/2016/02/image-loading-recyclerview-picasso.html
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class WorkbookActivityNewLan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workbook_new_lan);
+        initViews();
+    }
+
+    private void initViews() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.card_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 3);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<Language> language = prepareData();
+        DataAdapter adapter = new DataAdapter(getApplicationContext(), language);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private ArrayList<Language> prepareData() {
+
+        ArrayList<Language> language_array = new ArrayList<>();
+
+        for (int i = 0; i < language_names.length; i++) {
+            Language language = new Language();
+            language.setName(language_names[i]);
+            language.setImageURL(language_image_urls[i]);
+            language_array.add(language);
+        }
+        return language_array;
+    }
+
+    //data source
+    private final String language_names[] = {
+            "German",
+            "Chinese",
+            "Spanish",
+            "English",
+            "Finnish",
+            "Swedish",
+            "Japanese"
+
+    };
+
+    private final String language_image_urls[] = {
+            "http://julia-travel.com/uploads/vendor_category/7/184x184.gif",
+            "http://julia-travel.com/uploads/vendor_category/18/184x184.jpg",
+            "http://julia-travel.com/uploads/vendor_category/13/184x184.jpg",
+            "http://julia-travel.com/uploads/vendor_category/5/184x184.gif",
+            "http://www.fyidenmark.com/images/finland.gif",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6AxXAD36sHrlYU5Td8T_bte8wIknH5zLNvW9dfN2MDeRzfEa6",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT63xseKnCub6NoNJA6QL5QMCyWwAOMUFntVn1ivTHeC4XFMAuNfA"
+    };
+}
+
+
+
+
+
+
+
+
+
+
