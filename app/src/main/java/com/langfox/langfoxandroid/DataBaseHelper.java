7ff53d463ec63f1916495ab27158f161f4654bd4
jensmeister <G1t51b1t51>
2017-10-11 16:07:32 +0200

    Changes to be committed: modified:   app/src/main/java/com/langfox/langfoxandroid/DataBaseHelper.java modified:   app/src/main/java/com/langfox/langfoxandroid/SQLiteTestActivity.java

--- app/src/main/java/com/langfox/langfoxandroid/DataBaseHelper.java
+++ app/src/main/java/com/langfox/langfoxandroid/DataBaseHelper.java
@@ -1,10 +1,14 @@
 package com.langfox.langfoxandroid;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by pengchengliu on 05/10/2017.
@@ -76,6 +80,58 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    private String[] getFlagUrlsArray() {
+        List<String> flagUrls = this.getFlagUrls();
+        String[] flagUrlsArray = new String[flagUrls.size()];
+        flagUrlsArray = flagUrls.toArray(flagUrlsArray);
+        return flagUrlsArray;
+    }
+
+    public List<String> getFlagUrls() {
+        //DataBaseHelper myDB = new DataBaseHelper(this);//getApplicationContext()
+        Log.w("langfoxApp", "testSql 2");
+
+        SQLiteDatabase database = this.getWritableDatabase();
+
+        Log.w("langfoxApp", "testSql 2.2, db size: " + DatabaseUtils.queryNumEntries(database, "language"));
+        this.writeTableAndData(database);
+        Log.w("langfoxApp", "testSql 2.4, db size: " + DatabaseUtils.queryNumEntries(database, "language"));
+
+        Log.w("langfoxApp", "testSql 2.5");
+
+        database = this.getReadableDatabase();
+        Log.w("langfoxApp", "testSql 3");
+
+        Cursor cursor = database.query("language", new String[]{"language_id", "name", "native_name", "language_iso1"}, null, null, null, null, null);
+        //String[] languageNameArray = new String[cursor.getCount()];
+        Log.w("langfoxApp", "testSql 4, cursor.getCount(): " + cursor.getCount());
+        List<String> strings = new ArrayList<>();
+
+        int index = 0;
+        cursor.moveToFirst();
+        //while (cursor.isAfterLast() == false) {
+        List<String> flagUrls = new ArrayList<>();
+        while (!cursor.isAfterLast()) {
+            //languageNameArray[i] = cursor.getString(0);
+            //Log.w("langfoxApp", "testSql 4.5, index: " + index);
+            Log.w("langfoxApp", "testSql 4.7, index: " + cursor.toString());
+            Log.w("langfoxApp", "testSql 4.7, index: " + cursor.getInt(0) + " / " + cursor.getString(1) + " / " + cursor.getString(2) + " / " + cursor.getString(3));
+            Log.w("langfoxApp", "https://s3-eu-west-1.amazonaws.com/jwfirstbucket/img/flagUrls/" + cursor.getString(3) + ".svg");
+            String flagUrl = "https://s3-eu-west-1.amazonaws.com/jwfirstbucket/img/flagUrls/" + cursor.getString(3) + ".svg";
+            flagUrls.add(flagUrl);
+            //strings.add(cursor.getString(0));
+            strings.add("cat is dog");
+            //Log.w("langfoxApp", "testSql 5: " + cursor.getString(0));
+            cursor.moveToNext();
+            index++;
+        }
+        cursor.close();
+        database.close();
+
+        return flagUrls;
+
     }
 
 }
