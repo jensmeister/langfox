be6e867db935203cbeb8d5808e2b5e125228b0f6
PengInGitHub <felixdeutschland@hotmail.com>
2017-10-19 16:39:17 +0200

    change nothing but merge to master

--- app/src/main/java/com/langfox/langfoxandroid/WordbookFragment.java
+++ app/src/main/java/com/langfox/langfoxandroid/WordbookFragment.java
@@ -96,7 +96,7 @@ public class WordbookFragment extends Fragment implements AdapterView.OnItemClic
 
     }
 
-//lalala
+//lalalalala
     public WordbookFragment() {
         // Required empty public constructor
     }
